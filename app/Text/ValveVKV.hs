654e764f595971855b05bc004c7a57b5e37895a4
Bernardo Gomes Negri <38192358+BernardoGomesNegri@users.noreply.github.com>
2021-09-29 17:54:33 -0300

    add a tag and change export order

--- app/Text/ValveVKV.hs
+++ app/Text/ValveVKV.hs
@@ -1,5 +1,5 @@
-module Text.ValveVKV(vkvParser, parseValveVKV, fromValveVKV, (.:), (^:), unpair, ValveVKV,
-    ValveKeyValueEntry(KVObject, KVInt, KVString), Pair (Pair), Context, parseToVKV) where
+module Text.ValveVKV(parseValveVKV, parseToVKV, fromValveVKV, (.:), (^:), vkvParser, unpair, ValveVKV,
+    ValveKeyValueEntry(KVObject, KVInt, KVString), Pair (Pair), Context) where
 -- Library for processing Valve's value keyvalue format. The main function you will wish to use is parseValveVKV. To convert it into your own type, you
 -- will need to write a 'ValveVKV' instance for it.
 
@@ -19,5 +19,7 @@ parseValveVKV input =
             fromValveVKV topObj topObj
 
 -- | Parses it directly to a list of entries. Most of the times, 'parseValveVKV' will be better to directly turn it into a Haskell type of your choice
+--
+-- @since 1.0.1.0
 parseToVKV :: String -> Either ParseError [ValveKeyValueEntry]
 parseToVKV = parse vkvParser ""
